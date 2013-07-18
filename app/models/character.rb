a7a6dd2b55b4d285b8d09f8d2f3143e7bf9618c2
Hilke Ros <hilke@mmmotion.com>
2013-07-18 18:40:09 +0200

    mobile css

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,5 @@
 class Character < ActiveRecord::Base
   attr_accessible :name, :photo
+  validates :name, :presence => {:message => 'Geef minstens een naam in'}
   has_many :posts, dependent: :destroy
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   attr_accessible :body, :character_id
+  validates :body, :presence => {:message => 'Lege update, kan niet bewaard worden'}
   belongs_to :character
 end
