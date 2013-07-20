6539d26dd1cb104a710def2a5fa9a44b1ef39586
Hilke Ros <hilke@mmmotion.com>
2013-07-20 17:42:34 +0200

    added validation for character_id

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   attr_accessible :body, :character_id
   validates :body, :presence => {:message => 'Lege update, kan niet bewaard worden'}
+  validates :character_id, :presence => {:message => 'Geen personage geselecteerd'}
   belongs_to :character
 end
